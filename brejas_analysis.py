bbddca1f7515fdf646394cb31db8aaf893ba43f2
deniseiras <denis.eiras@gmail.com>
2024-04-28 11:20:15 -0300

    preprocess types; starting sanitizing columns

--- brejas_analysis.py
+++ brejas_analysis.py
@@ -46,7 +46,25 @@ def generate_descriptive_statistics(df, file_to_save):
     return statistics
 
 def preprocess_columns(df):
-    df['year'] = pd.to_datetime(df['review_datetime']).dt.year
+    df['review_datetime'] = pd.to_datetime(df['review_datetime'])
+    df['beer_alcohol'] = df['beer_alcohol'].str.replace('% ABV', '').astype(float)
+    df['beer_srm'] = df['beer_srm'].str.replace('.', '').str.replace(',', '.').astype(float)
+    for field in ['review_aroma', 'review_visual', 'review_flavor', 'review_sensation', 'review_general_set']:
+        df[field] = df[field].apply(eval).astype(float)
+        df[field] = df[field] * 5
+    return df
+
+def sanitize_column(df, column_name, min_val, max_val):
+    invalid_data = df[(df[column_name] > max_val) | (df[column_name] < min_val)]
+    print(f'Removing {len(invalid_data)} lines of invalid {column_name}')
+    df = df.drop(invalid_data.index)
+    return df
+
+
+def sanitize_data(df):
+        
+    df = sanitize_column(df, 'beer_srm', 0, 80)    
+            
     return df
 
                        
@@ -59,14 +77,12 @@ def main():
     
     df = read_data(file)
     df = preprocess_columns(df)
-
-    df_tmp = select_complete_rows(df)
-    file_complete = f'{data_folder}/complete.csv'
-    df_tmp.to_csv(file_complete)
+    print(df.dtypes)
     
-    file_complete_stats = f'{data_folder}/complete_stats.csv'
-    _ = generate_descriptive_statistics(df_tmp, file_complete_stats)
-    
+    # TODO CALL after select_rows_with_comment
+    df = sanitize_data(df)
+        
+    return 1
     df_tmp = select_rows_with_comment(df)
     file_wcomments = f'{data_folder}/wcommnets.csv'
     df_tmp.to_csv(file_wcomments)
@@ -74,10 +90,16 @@ def main():
     file_wcomments_stats = f'{data_folder}/wcommnets_stats.csv'
     _ = generate_descriptive_statistics(df_tmp, file_wcomments_stats)
     
-    
-    df_distinct_styles_year = df.groupby(['beer_style', 'year']).nunique()
+    df_distinct_styles_year = df.groupby(['beer_style', df['review_datetime'].dt.year]).nunique()
     df_distinct_styles_year.to_csv(f'{data_folder}/disitinct_styles_year.csv')
     
 
 if __name__ == "__main__":
     main()
+
+
+    # # df_tmp = select_complete_rows(df)
+    # # file_complete = f'{data_folder}/complete.csv'
+    # # df_tmp.to_csv(file_complete)
+    # file_complete_stats = f'{data_folder}/complete_stats.csv'
+    # _ = generate_descriptive_statistics(df_tmp, file_complete_stats)
