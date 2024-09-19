02de0a756cff4bee330149228963ce05a4026c56
deniseiras <denis.eiras@gmail.com>
2024-09-19 18:20:56 -0300

    step 6 - removing filter threshold

--- absa_beer/step_6.py
+++ absa_beer/step_6.py
@@ -54,38 +54,45 @@ class Step_6(Step):
         print(f'- Base ABSA      - line count: {len(df_base_absa)}')
         print(f'- Base Joined    - line count: {len(df_absa_join)}')
         
-        # df_aroma_pos, df_aroma_neg = self.create_base(df_absa_join, 'review_aroma', 'aroma')
-        # df_visual_pos, df_visual_neg = self.create_base(df_absa_join, 'review_visual', 'visual')
-        # df_flavor_pos, df_flavor_neg = self.create_base(df_absa_join, 'review_flavor', 'sabor')
-        # df_sensation_pos, df_sensation_neg = self.create_base(df_absa_join, 'review_sensation', 'sensação na boca')
-        df_general_set_pos, df_general_set_neg = self.create_base(df_absa_join, 'review_general_set', None)
+        df_aroma_pos, df_aroma_neg = self.create_base(df_absa_join, 'aroma')
+        df_visual_pos, df_visual_neg = self.create_base(df_absa_join, 'visual')
+        df_flavor_pos, df_flavor_neg = self.create_base(df_absa_join, 'sabor')
+        df_sensation_pos, df_sensation_neg = self.create_base(df_absa_join, 'sensação na boca')
+        
+        # include all categories, also alcool, amargor
+        df_all_cats_pos, df_all_cats_neg = self.create_base(df_absa_join)
         
         stop_words = self.get_stop_words()
-        # self.generate_word_cloud(df_aroma_pos, 'aroma_pos', stop_words)
-        # self.generate_word_cloud(df_aroma_neg, 'aroma_neg', stop_words)
-        # self.generate_word_cloud(df_visual_pos, 'visual_pos', stop_words)
-        # self.generate_word_cloud(df_visual_neg, 'visual_neg', stop_words)
-        # self.generate_word_cloud(df_flavor_pos, 'flavor_pos', stop_words)
-        # self.generate_word_cloud(df_flavor_neg, 'flavor_neg', stop_words)
-        # self.generate_word_cloud(df_sensation_pos, 'sensation_pos', stop_words)
-        # self.generate_word_cloud(df_sensation_neg, 'sensation_neg', stop_words)
-        self.generate_word_cloud(df_general_set_pos, 'general_set_pos', stop_words, categories, max_words=75, split_words=False)
-        self.generate_word_cloud(df_general_set_neg, 'general_set_neg', stop_words, categories, max_words=75, split_words=False)
+        max_words = 75
+        split_words = False
+        self.generate_word_cloud(df_aroma_pos, 'aroma_pos', stop_words, categories, max_words=max_words, split_words=split_words)
+        self.generate_word_cloud(df_aroma_neg, 'aroma_neg', stop_words, categories, max_words=max_words, split_words=split_words)
+        self.generate_word_cloud(df_visual_pos, 'visual_pos', stop_words, categories, max_words=max_words, split_words=split_words)
+        self.generate_word_cloud(df_visual_neg, 'visual_neg', stop_words, categories, max_words=max_words, split_words=split_words)
+        self.generate_word_cloud(df_flavor_pos, 'flavor_pos', stop_words, categories, max_words=max_words, split_words=split_words)
+        self.generate_word_cloud(df_flavor_neg, 'flavor_neg', stop_words, categories, max_words=max_words, split_words=split_words)
+        self.generate_word_cloud(df_sensation_pos, 'sensation_pos', stop_words, categories, max_words=max_words, split_words=split_words)
+        self.generate_word_cloud(df_sensation_neg, 'sensation_neg', stop_words, categories, max_words=max_words, split_words=split_words)
+        
+        self.generate_word_cloud(df_all_cats_pos, 'all_cats_pos', stop_words, categories, max_words=max_words, split_words=split_words)
+        self.generate_word_cloud(df_all_cats_neg, 'all_cats_neg', stop_words, categories, max_words=max_words, split_words=split_words)
 
 
-    def create_base(self, df_absa_join, column: str, category: str ):
+    def create_base(self, df_absa_join, category:str = None ):  # column: str ):
         """
         This function creates a base with the desired column (aroma, visual, flavor, sensation, general_set)
         
         Args:
                 df_absa_join (DataFrame): The dataframe with ABSA sentiments
         """
-        pos_thres = 0.0  # 0.0 ignore rating 
-        neg_thres = 5.0  # 5.0 ignore rating
-        print(column.capitalize())
-        df_absa_join_rev_pos = df_absa_join[df_absa_join[column] >= pos_thres]
-        df_absa_join_rev_neg = df_absa_join[df_absa_join[column] <= neg_thres]
-        print(f'- Reviews         POS / NEG: {len(df_absa_join_rev_pos)} / {len(df_absa_join_rev_neg)}')
+        # Not using thresholds anymore
+        #
+        # pos_thres = 0.0  # 0.0 ignore rating 
+        # neg_thres = 5.0  # 5.0 ignore rating
+        # print(column.capitalize())
+        # df_absa_join_rev_pos = df_absa_join[df_absa_join[column] >= pos_thres]
+        # df_absa_join_rev_neg = df_absa_join[df_absa_join[column] <= neg_thres]
+        # print(f'- Reviews         POS / NEG: {len(df_absa_join_rev_pos)} / {len(df_absa_join_rev_neg)}')
         
         df_absa_join_absa_pos = df_absa_join[
             (df_absa_join['sentiment'].isin(['positivo', 'muito positivo'])) ]            
@@ -96,25 +103,38 @@ class Step_6(Step):
             df_absa_join_absa_neg = df_absa_join_absa_neg[df_absa_join_absa_neg['category'] == category]
         print(f'- ABSA            POS / NEG: {len(df_absa_join_absa_pos)} / {len(df_absa_join_absa_neg)}')
         
-        df_pos = df_absa_join[
-            (df_absa_join[column] > pos_thres) & 
-            (df_absa_join['sentiment'].isin(['positivo', 'muito positivo'])) ]
+        # Not using thresholds anymore
+        #
+        # df_pos = df_absa_join[
+        #     (df_absa_join[column] > pos_thres) & 
+        #     (df_absa_join['sentiment'].isin(['positivo', 'muito positivo'])) ]
                 
-        df_neg = df_absa_join[
-            (df_absa_join[column] <= neg_thres) & 
-            (df_absa_join['sentiment'].isin(['negativo', 'muito negativo'])) ]
+        # df_neg = df_absa_join[
+        #     (df_absa_join[column] <= neg_thres) & 
+        #     (df_absa_join['sentiment'].isin(['negativo', 'muito negativo'])) ]
         
-        if category is not None:
-            df_pos = df_pos[df_pos['category'] == category]
-            df_neg = df_neg[df_neg['category'] == category]
+        # if category is not None:
+        #     df_pos = df_pos[df_pos['category'] == category]
+        #     df_neg = df_neg[df_neg['category'] == category]
         
-        df_pos.to_csv(f'{self.work_dir}/step_6_{column}_POS.csv', index=False)
-        df_neg.to_csv(f'{self.work_dir}/step_6_{column}_NEG.csv', index=False)
-        
+        df_pos = df_absa_join_absa_pos
+        df_neg = df_absa_join_absa_neg
         print(f'- Reviews & ABSA  POS / NEG: {len(df_pos)} / {len(df_neg)}')
         
-        return df_pos, df_neg
+        if category is None:
+            categ_str = 'all_cats'
+        else:
+            categ_str = category
+        self.save_df_pos_neg(categ_str, df_pos, df_neg)
         
+        return df_pos, df_neg
+
+
+    def save_df_pos_neg(self, base_name, df_pos, df_neg):
+        df_pos.to_csv(f'{self.work_dir}/step_6_{base_name}_POS.csv', index=False)
+        df_neg.to_csv(f'{self.work_dir}/step_6_{base_name}_NEG.csv', index=False)
+        
+    
     def get_stop_words(self):
         from nltk.corpus import stopwords
         import nltk
@@ -126,8 +146,10 @@ class Step_6(Step):
             'eu particularmente não gostei', 'não gostei', 'não me agradaram muito', 'me decepcionou', 'não é ruim', 'é muito bom', 
             'não é muito bom', 'não é muito boa' 'é ruim', 'é muito ruim', 'não é muito ruim', 'não é bom', 'não é boa' 'é bom',
             'adorei', 'gostosa', 'agradável', 'esperava um pouco mais', 'esperava muito mais', 'custo benefício ruim', 'é meio estranha',
-            'notas fortes', 'não empolga', 'cerveja bem mediana' 'cheiro apagado'])
-        print(stop_words)
+            'notas fortes', 'não empolga', 'cerveja bem mediana' 'cheiro apagado', 'sabor agradável', 'muito gostosa', 'saborosa',
+            'cerveja bem mediana', 'cerveja muito ruim', 'desequilibrado', 'desequilibrada' 'desbalanceada', 'desbalanceado', 'pena ser pequena',
+            'confuso', 'cerveja muito ruim', 'não foi bacana'])
+        # print(stop_words)
        
         # print(stop_words)
         return stop_words
@@ -151,10 +173,6 @@ class Step_6(Step):
             word_category = word_category_mapping.get(word, 'default')
             return category_colors.get(word_category, 'black')
 
-        # # Create a list of unique categories and assign a color to each
-        # cmap = get_cmap('tab10', len(categories))  # Get a colormap with enough colors for each category
-        # category_colors = {category: rgb2hex(cmap(i)) for i, category in enumerate(categories)}
-        
         category_colors = {
             "visual": "Blue",
             "aroma": "Orange",
@@ -164,7 +182,7 @@ class Step_6(Step):
             "amargor": "Lime"
         }
         # Convert the color names to hex codes
-        category_colors_hex = {category: to_hex(color) for category, color in category_colors.items()}
+        # category_colors_hex = {category: to_hex(color) for category, color in category_colors.items()}
 
         # Create a mapping between words and their corresponding category
         word_category_mapping = {}
@@ -176,6 +194,8 @@ class Step_6(Step):
         # Apply extraction and count words
         word_list = df['aspect'].apply(extract_entities).sum()
         word_count = Counter(word_list)
+        # print(f'Word count for {base_name}:')
+        # print(word_count)
         
         category_word_count = Counter(word_category_mapping.values())
         print(f'Number of words per category for {base_name}:')
